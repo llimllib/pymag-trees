03fdc2941ac1d7ac9a0d6c3dfec7de29f12b12a9
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2008-03-01 01:47:06 +0000

    Updated to use drawtree
    git-svn-id: http://billmill.org/svn/personal_code/python/trees@103 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- reingold.py
+++ reingold.py
@@ -2,29 +2,39 @@ from gen import Tree, gentree
 from operator import lt, gt
 from sys import stdout
 
+class DrawTree:
+    def __init__(self, tree, depth=-1):
+        self.x = -1
+        self.y = depth
+        self.tree = tree
+        self.children = []
+
 #traverse to the bottom of the tree, and place the leaves at an arbitrary
 #   x coordinate
 #if the node is a parent, draw its subtrees, then shift the right one as close
 #   to the left as possible
 #place the parent in the middle of the two trees.
 def reingold_tilford(tree, depth=0):
-    tree.y = depth
+    dt = DrawTree(tree, depth)
     if len(tree) == 0:
-        tree.x = 0
-        return tree
+        dt.x = 0
+        return dt
 
     if len(tree) == 1:
-        tree.x = (reingold_tilford(tree, depth+1)).x
-        return tree
+        dt.children = [reingold_tilford(tree[0], depth+1)]
+        dt.x = dt.children[0].x
+        return dt
 
     left = reingold_tilford(tree[0], depth+1)
     right = reingold_tilford(tree[1], depth+1)
 
-    tree.x = fix_subtrees(left, right)
-    return tree
+    dt.children = [left, right]
+    dt.x = fix_subtrees(left, right)
+    return dt
 
 #place the right subtree as close to the left subtree as possible
 def fix_subtrees(left, right):
+    #import pdb; pdb.set_trace()
     wl = contour(left, lt)
     wr = contour(right, gt)
     diff = max(map(lambda (x,y): x-y, zip(wl,wr))) + 1
@@ -35,8 +45,8 @@ def fix_subtrees(left, right):
 
 def addtotree(tree, val):
     tree.x += val
-    if len(tree):
-        for child in tree[:2]: addtotree(child, val)
+    if len(tree.children):
+        for child in tree.children[:2]: addtotree(child, val)
     
 def contour(tree, comp, level=0, cont=None):
     if not level: 
@@ -53,7 +63,7 @@ def contour(tree, comp, level=0, cont=None):
 
 #given an array of nodes, print them out reasonably on one line
 def printrow(level):
-    x = dict((t.x, t) for t in level)
+    x = dict((t.x, t.tree) for t in level)
     for i in range(max(x.keys())+1):
         try: stdout.write(str(x[i])[:4])
         except: stdout.write("    ")
@@ -65,12 +75,12 @@ def p(tree):
         printrow(level)
         for t in level:
             #stdout.write("%s%s%s" % (t.x*"____", str(t)[:3], t.x))
-            newlevel.extend(t[:2])
+            newlevel.extend(t.children[:2])
         print
         if not newlevel: break
         level = newlevel
 
 root = gentree("/Users/llimllib/Movies")
 root.children.reverse()
-reingold_tilford(root)
-p(root)
+drawtree = reingold_tilford(root)
+p(drawtree)
