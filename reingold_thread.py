556beeafbd126c6c9d84e4efed56a2bfe63bb6fb
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2008-03-03 05:21:05 +0000

    threaded version working
    git-svn-id: http://billmill.org/svn/personal_code/python/trees@107 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- reingold_thread.py
+++ reingold_thread.py
@@ -8,6 +8,13 @@ class DrawTree:
         self.y = depth
         self.tree = tree
         self.children = []
+        self.thread = None
+
+    def left(self): 
+        return self.thread or len(self.children) and self.children[0]
+
+    def right(self):
+        return self.thread or len(self.children) and self.children[-1]
 
 #traverse to the bottom of the tree, and place the leaves at an arbitrary
 #   x coordinate
@@ -34,32 +41,44 @@ def reingold_tilford(tree, depth=0):
 
 #place the right subtree as close to the left subtree as possible
 def fix_subtrees(left, right):
-    #import pdb; pdb.set_trace()
-    wl = contour(left, lt)
-    wr = contour(right, gt)
+    wl = contour(left, 'right')
+    wr = contour(right, 'left')
+    if len(wl) != len(wr):
+        fix_threads(left, right, len(wl), len(wr))
+
     diff = max(map(lambda (x,y): x-y, zip(wl,wr))) + 1
     #stick to the integers
     diff += (right.x + diff + left.x) % 2
+
     addtotree(right, diff)
     return (left.x + right.x) / 2
+
+def fix_threads(left, right, ldepth, rdepth):
+    if ldepth > rdepth: rattach(left, right, rdepth)
+    else:               lattach(left, right, ldepth)
+
+#when the left tree is deeper than the right tree, descend rdepth levels
+#down the right side of both trees and attach the last node on the right
+#to the rightmost node on the left
+def rattach(left, right, depth):
+    if depth > 1:
+        return rattach(left.right(), right.right(), depth-1)
+    right.thread = left.right()
+
+def lattach(left, right, depth):
+    if depth > 1:
+        return rattach(left.left(), right.left(), depth-1)
+    left.thread = left.left()
 
 def addtotree(tree, val):
     tree.x += val
     if len(tree.children):
         for child in tree.children[:2]: addtotree(child, val)
-    
-def contour(tree, comp, level=0, cont=None):
-    if not level: 
-        cont = [tree.x]
-    elif len(cont) < level+1:
-        cont.append(tree.x)
-    elif comp(cont[level], tree.x):
-        cont[level] = tree.x
-
-    for child in tree.children[:2]:
-        contour(child, comp, level+1, cont)
-
-    return cont
+ 
+def contour(tree, next): 
+    next_node = getattr(tree, next)()
+    if not next_node: return [tree.x]
+    else:             return [tree.x] + contour(next_node, next)
 
 #given an array of nodes, print them out reasonably on one line
 def printrow(level):
@@ -74,7 +93,6 @@ def p(tree):
         newlevel = []
         printrow(level)
         for t in level:
-            #stdout.write("%s%s%s" % (t.x*"____", str(t)[:3], t.x))
             newlevel.extend(t.children[:2])
         print
         if not newlevel: break
