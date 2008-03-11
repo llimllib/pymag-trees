3d7a3966ad473b8d7a39cd1a45e3b5b6954a16a7
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2008-03-11 02:33:27 +0000

    more testing
    git-svn-id: http://billmill.org/svn/personal_code/python/trees@113 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- test_tree.py
+++ test_tree.py
@@ -3,14 +3,6 @@ from reingold_thread import reingold_tilford as rtc
 import unittest
 
 class TestDrawTree(unittest.TestCase):
-    def testSimple(self):
-        tree = Tree("root", [
-                 Tree("l"), 
-                 Tree("r")])
-        dt = self.f(tree)
-
-        self.assertTree(dt, [1, [0, 2]])
-
     def assertTree(self, tree, expected):
         self.assertEqual(tree.x, expected[0])
         for i in range(1, len(tree.children)):
@@ -18,6 +10,14 @@ class TestDrawTree(unittest.TestCase):
             exp = expected[i]
             if isinstance(exp, int): self.assertTree(child, [exp])
             else:                    self.assertTree(child, exp)
+
+    def testSimple(self):
+        tree = Tree("root", [
+                 Tree("l"), 
+                 Tree("r")])
+        dt = self.f(tree)
+
+        self.assertTree(dt, [1, [0, 2]])
 
     def testDeepLeft(self):
         tree = Tree("root", [
@@ -33,14 +33,30 @@ class TestDrawTree(unittest.TestCase):
 
     def testDeepRight(self):
         tree = Tree("root", [
-                 Tree("r1"),
-                 Tree("l1", [
-                   Tree("l2", [
-                     Tree("l3", [
-                       Tree("l4")])])])
+                 Tree("l1"),
+                 Tree("r1", [
+                   Tree("r2", [
+                     Tree("r3", [
+                       Tree("r4")])])])
                  ])
         dt = self.f(tree)
         expected = [1, 0, [2, [2, [2, [2]]]]]
+
+        self.assertTree(dt, expected)
+
+    def testBalanced(self):
+        """It helps to draw these out on paper and have an editor that matches
+        parentheses :)"""
+        tree = Tree("root", [
+                 Tree("l1", [
+                   Tree("l2", [
+                     Tree("l3"), Tree("l4")])]),
+                 Tree("r1", [
+                   Tree("rl1"),
+                   Tree("rr1", [
+                     Tree("rr2"), Tree("rr3")])])])
+        dt = self.f(tree)
+        expected = [2, [1, [1, [0, 2]]], [3, [2], [4, [3, 5]]]]
 
         self.assertTree(dt, expected)
 
