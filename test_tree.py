6c4cd6d7f73fe1d2c1eed2b9981222d697717358
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2008-03-26 19:17:05 +0000

    fixing the testing function to actually work
    git-svn-id: http://billmill.org/svn/personal_code/python/trees@119 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- test_tree.py
+++ test_tree.py
@@ -1,11 +1,12 @@
 from gen import Tree
 from reingold_thread import reingold_tilford as rtc
+from reingold_thread import p as printtree
 import unittest
 
 class TestDrawTree(unittest.TestCase):
     def assertTree(self, tree, expected):
         self.assertEqual(tree.x, expected[0])
-        for i in range(1, len(tree.children)):
+        for i in range(1, len(tree.children)+1):
             child = tree.children[i-1]
             exp = expected[i]
             if isinstance(exp, int): self.assertTree(child, [exp])
@@ -17,7 +18,7 @@ class TestDrawTree(unittest.TestCase):
                  Tree("r")])
         dt = self.f(tree)
 
-        self.assertTree(dt, [1, [0, 2]])
+        self.assertTree(dt, [1, 0, 2])
 
     def testDeepLeft(self):
         tree = Tree("root", [
@@ -40,7 +41,7 @@ class TestDrawTree(unittest.TestCase):
                        Tree("r4")])])])
                  ])
         dt = self.f(tree)
-        expected = [1, 0, [2, [2, [2, [2]]]]]
+        expected = [1, 0, [2, [2, [2, 2]]]]
 
         self.assertTree(dt, expected)
 
@@ -56,7 +57,31 @@ class TestDrawTree(unittest.TestCase):
                    Tree("rr1", [
                      Tree("rr2"), Tree("rr3")])])])
         dt = self.f(tree)
-        expected = [2, [1, [1, [0, 2]]], [3, [2], [4, [3, 5]]]]
+        expected = [2, [1, [1, 0, 2]], [3, 2, [4, 3, 5]]]
+
+        self.assertTree(dt, expected)
+
+    def testUnbalanced(self):
+        #this one is a portion of a category tree from work;
+        #it tests threads pretty strenuously. There should probably
+        #be a thread-specific test.
+        tree = Tree("root", [
+                 Tree("l1", [
+                   Tree("l2", [
+                     Tree("l3", [
+                       Tree("l4", [
+                         Tree("l5"),
+                         Tree("l6")]),
+                       Tree("l7")]),
+                     Tree("l8")]),
+                   Tree("l9")]),
+                 Tree("r1", [
+                   Tree("r2", [
+                     Tree("r3"),
+                     Tree("r4")]),
+                   Tree("r5")])])
+        dt = self.f(tree)
+        expected = [6, [4, [3, [2, [1, 0, 2], 3], 4], 5], [8, [7, 6, 8], 9]]
 
         self.assertTree(dt, expected)
 
@@ -81,5 +106,5 @@ if __name__ == "__main__":
     load = unittest.TestLoader().loadTestsFromTestCase
     suite = load(TestNaive)
     suite.addTests(load(TestThread))
-    suite.addTests(load(TestAddMod))
+    #suite.addTests(load(TestAddMod))
     unittest.TextTestRunner(verbosity=2).run(suite)
