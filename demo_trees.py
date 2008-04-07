15db4eb5f86e2be52a39d17298839f075ae4502b
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2008-04-07 02:48:30 +0000

    moving test trees to their own module
    git-svn-id: http://billmill.org/svn/personal_code/python/trees@143 c4aad215-931b-0410-ba74-c4b90e0b6ad6

---
+++ demo_trees.py
@@ -0,0 +1,62 @@
+from gen import Tree
+
+trees = [
+Tree("root", 
+     Tree("l"), 
+     Tree("r")),
+
+Tree("root", 
+  Tree("l1", 
+    Tree("l2", 
+      Tree("l3", 
+        Tree("l4")))),
+  Tree("r1")),
+
+Tree("root", 
+  Tree("l1"),
+  Tree("r1", 
+    Tree("r2", 
+      Tree("r3", 
+        Tree("r4"))))
+  ),
+
+Tree("root", 
+  Tree("l1", 
+    Tree("l2", 
+      Tree("l3"), Tree("l4"))),
+  Tree("r1", 
+    Tree("rl1"),
+    Tree("rr1", 
+      Tree("rr2"), Tree("rr3")))),
+
+Tree("root", 
+  Tree("l1", 
+    Tree("l2", 
+      Tree("l3", 
+        Tree("l4", 
+          Tree("l5"),
+          Tree("l6")),
+        Tree("l7")),
+      Tree("l8")),
+    Tree("l9")),
+  Tree("r1", 
+    Tree("r2", 
+      Tree("r3"),
+      Tree("r4")),
+    Tree("r5"))),
+
+Tree("root",
+  Tree("l1",
+    Tree("ll1"),
+    Tree("lr1",
+      Tree("lrl"),
+      Tree("lrr"))),
+  Tree("r1",
+    Tree("rr2",
+      Tree("rr3",
+        Tree("rrl",
+          Tree("rrll",
+            Tree("rrlll"),
+            Tree("rrllr")),
+          Tree("rrlr"))))))
+]
