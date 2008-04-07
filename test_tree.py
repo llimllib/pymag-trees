f553a34a19b4495504644490691cb745954d87c1
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2008-04-07 02:49:38 +0000

    modifying to use demo_trees
    git-svn-id: http://billmill.org/svn/personal_code/python/trees@144 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- test_tree.py
+++ test_tree.py
@@ -1,4 +1,5 @@
 from gen import Tree
+from demo_trees import trees
 from reingold_thread import reingold_tilford as rtc
 from reingold_thread import p as printtree
 import unittest
@@ -13,77 +14,38 @@ class TestDrawTree(unittest.TestCase):
             else:                    self.assertTree(child, exp)
 
     def testSimple(self):
-        tree = Tree("root", [
-                 Tree("l"), 
-                 Tree("r")])
-        dt = self.f(tree)
-
+        dt = self.f(trees[0])
         self.assertTree(dt, [1, 0, 2])
 
     def testDeepLeft(self):
-        tree = Tree("root", [
-                 Tree("l1", [
-                   Tree("l2", [
-                     Tree("l3", [
-                       Tree("l4")])])]),
-                 Tree("r1")])
-        dt = self.f(tree)
+        dt = self.f(trees[1])
         expected = [1, [0, [0, [0, [0]]]], 2]
-
         self.assertTree(dt, expected)
 
     def testDeepRight(self):
-        tree = Tree("root", [
-                 Tree("l1"),
-                 Tree("r1", [
-                   Tree("r2", [
-                     Tree("r3", [
-                       Tree("r4")])])])
-                 ])
-        dt = self.f(tree)
+        dt = self.f(trees[2])
         expected = [1, 0, [2, [2, [2, 2]]]]
-
         self.assertTree(dt, expected)
 
     def testBalanced(self):
-        #It helps to draw these out on paper and have an editor that matches
-        #parentheses :)
-        tree = Tree("root", [
-                 Tree("l1", [
-                   Tree("l2", [
-                     Tree("l3"), Tree("l4")])]),
-                 Tree("r1", [
-                   Tree("rl1"),
-                   Tree("rr1", [
-                     Tree("rr2"), Tree("rr3")])])])
-        dt = self.f(tree)
+        dt = self.f(trees[3])
         expected = [2, [1, [1, 0, 2]], [3, 2, [4, 3, 5]]]
-
         self.assertTree(dt, expected)
 
     def testUnbalanced(self):
         #this one is a portion of a category tree from work;
         #it tests threads pretty strenuously. There should probably
         #be a thread-specific test.
-        tree = Tree("root", [
-                 Tree("l1", [
-                   Tree("l2", [
-                     Tree("l3", [
-                       Tree("l4", [
-                         Tree("l5"),
-                         Tree("l6")]),
-                       Tree("l7")]),
-                     Tree("l8")]),
-                   Tree("l9")]),
-                 Tree("r1", [
-                   Tree("r2", [
-                     Tree("r3"),
-                     Tree("r4")]),
-                   Tree("r5")])])
-        dt = self.f(tree)
+        dt = self.f(trees[4])
         expected = [6, [4, [3, [2, [1, 0, 2], 3], 4], 5], [8, [7, 6, 8], 9]]
-
         self.assertTree(dt, expected)
+
+    def testWSTree(self):
+        #this is the tree featured prominently in Wetherell-Shannon. I draw it
+        #slightly differently, since I don't have strong right-left trees, we draw
+        #single children directly underneath parents
+        dt = self.f(trees[5])
+        expected = [3, [2, 1, [3, 2, 4]], [6, [6, [6, [6, [5, [4, 3, 5], 6]]]]]]
 
 class TestNaive(TestDrawTree):
     def setUp(self): 
@@ -102,9 +64,15 @@ class TestAddMod(TestDrawTree):
         import reingold_addmod
         self.f = reingold_addmod.reingold_tilford
 
+class TestBuchheim(TestDrawTree):
+    def setUp(self):
+        import buchheim
+        self.f = buchheim.buchheim
+
 if __name__ == "__main__":
     load = unittest.TestLoader().loadTestsFromTestCase
     suite = load(TestNaive)
     suite.addTests(load(TestThread))
     #suite.addTests(load(TestAddMod))
+    suite.addTests(load(TestBuchheim))
     unittest.TextTestRunner(verbosity=2).run(suite)
