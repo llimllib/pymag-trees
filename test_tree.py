f1b750349c38c16f0d20cb1d620fb31d8631e0b8
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2008-04-12 01:19:38 +0000

    Added one test and a skeleton for n-ary tree tests
    git-svn-id: http://billmill.org/svn/personal_code/python/trees@165 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- test_tree.py
+++ test_tree.py
@@ -4,34 +4,33 @@ from reingold_thread import reingold_tilford as rtc
 from reingold_thread import p as printtree
 import unittest
 
-class TestDrawTree(unittest.TestCase):
-    def assertTree(self, tree, expected):
-        self.assertEqual(tree.x, expected[0])
-        print tree.x, expected[0]
-        for i in range(1, len(tree.children)+1):
-            child = tree.children[i-1]
-            exp = expected[i]
-            if isinstance(exp, int): self.assertTree(child, [exp])
-            else:                    self.assertTree(child, exp)
+def assertTree(self, tree, expected):
+    self.assertEqual(tree.x, expected[0])
+    for i in range(1, len(tree.children)+1):
+        child = tree.children[i-1]
+        exp = expected[i]
+        if isinstance(exp, int): assertTree(child, [exp])
+        else:                    assertTree(child, exp)
 
+class TestBinTree(unittest.TestCase):
     def testSimple(self):
         dt = self.f(trees[0])
-        self.assertTree(dt, [1, 0, 2])
+        assertTree(dt, [1, 0, 2])
 
     def testDeepLeft(self):
         dt = self.f(trees[1])
         expected = [1, [0, [0, [0, [0]]]], 2]
-        self.assertTree(dt, expected)
+        assertTree(dt, expected)
 
     def testDeepRight(self):
         dt = self.f(trees[2])
         expected = [1, 0, [2, [2, [2, 2]]]]
-        self.assertTree(dt, expected)
+        assertTree(dt, expected)
 
     def testBalanced(self):
         dt = self.f(trees[3])
         expected = [2, [1, [1, 0, 2]], [3, 2, [4, 3, 5]]]
-        self.assertTree(dt, expected)
+        assertTree(dt, expected)
 
     def testUnbalanced(self):
         #this one is a portion of a category tree from work;
@@ -39,7 +38,7 @@ class TestDrawTree(unittest.TestCase):
         #be a thread-specific test.
         dt = self.f(trees[4])
         expected = [6, [4, [3, [2, [1, 0, 2], 3], 4], 5], [8, [7, 6, 8], 9]]
-        self.assertTree(dt, expected)
+        assertTree(dt, expected)
 
     def testWSTree(self):
         #this is the tree featured prominently in Wetherell-Shannon. I draw it
@@ -47,26 +46,39 @@ class TestDrawTree(unittest.TestCase):
         #single children directly underneath parents
         dt = self.f(trees[5])
         expected = [3, [1, 0, [2, 1, 3]], [5, [5, [5, [5, [4, 3, 5], 6]]]]]
-        self.assertTree(dt, expected)
+        assertTree(dt, expected)
 
-class TestNaive(TestDrawTree):
+    def testBuchMerge(self):
+        #the simplest tree I could make fail on buchheim's algo
+        dt = self.f(trees[6])
+        expected = [3, [1, 0, 2], [5, 4, 6]]
+        assertTree(dt, expected)
+
+def testNaryTree(unittest.TestCase):
+    def testBuchheim(self):
+        #Figure 2 in buchheim
+        dt = self.f(trees[7])
+        expected = pass #?
+                        #we'll need to figure stuff out before testing this.
+
+class TestNaive(TestBinTree):
     def setUp(self): 
         import reingold_naive
         self.f = reingold_naive.reingold_tilford
 
-class TestThread(TestDrawTree):
+class TestThread(TestBinTree):
     #TODO: My current tests didn't make the lattach bug fail... write a test
     #      that does. (See r116, reingold_thread.py)
     def setUp(self): 
         import reingold_thread
         self.f = reingold_thread.reingold_tilford
 
-class TestAddMod(TestDrawTree):
+class TestAddMod(TestBinTree):
     def setUp(self): 
         import reingold_addmod
         self.f = reingold_addmod.reingold_tilford
 
-class TestBuchheim(TestDrawTree):
+class TestBuchheim(TestBinTree):
     def setUp(self):
         import buchheim
         self.f = buchheim.buchheim
