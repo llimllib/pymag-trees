bcc9d5ea14c1c06a50e00d21b9fe245ea71eccc1
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2008-04-12 01:19:23 +0000

    Added a few trees
    git-svn-id: http://billmill.org/svn/personal_code/python/trees@164 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- demo_trees.py
+++ demo_trees.py
@@ -1,10 +1,12 @@
 from gen import Tree
 
 trees = [
+#0 simple test
 Tree("root", 
      Tree("l"), 
      Tree("r")),
 
+#1 deep left
 Tree("root", 
   Tree("l1", 
     Tree("l2", 
@@ -12,6 +14,7 @@ Tree("root",
         Tree("l4")))),
   Tree("r1")),
 
+#2 deep right
 Tree("root", 
   Tree("l1"),
   Tree("r1", 
@@ -20,6 +23,7 @@ Tree("root",
         Tree("r4"))))
   ),
 
+#3 balanced
 Tree("root", 
   Tree("l1", 
     Tree("l2", 
@@ -29,6 +33,7 @@ Tree("root",
     Tree("rr1", 
       Tree("rr2"), Tree("rr3")))),
 
+#4 unbalanced
 Tree("root", 
   Tree("l1", 
     Tree("l2", 
@@ -45,6 +50,7 @@ Tree("root",
       Tree("r4")),
     Tree("r5"))),
 
+#5 Wetherell-Shannon Tree
 Tree("root",
   Tree("l1",
     Tree("ll1"),
@@ -58,5 +64,37 @@ Tree("root",
           Tree("rrll",
             Tree("rrlll"),
             Tree("rrllr")),
-          Tree("rrlr"))))))
+          Tree("rrlr")))))),
+
+#6 Buchheim Failure
+Tree("root", 
+  Tree("l1",
+    Tree("l9"),
+      Tree("l10")),
+  Tree("r1", 
+    Tree("r2"),
+    Tree("r5"))),
+
+#6 buchheim n-ary tree
+Tree("root",
+  Tree("bigleft",
+    Tree("l1"),
+    Tree("l2"),
+    Tree("l3"),
+    Tree("l4"),
+    Tree("l5"),
+    Tree("l6"),
+    Tree("l7", Tree("ll1"))),
+  Tree("r1"),
+  Tree("r2"),
+  Tree("r3", Tree("r31")),
+  Tree("bigright",
+    Tree("brr",
+      Tree("br1"),
+      Tree("br2"),
+      Tree("br3"),
+      Tree("br4"),
+      Tree("br5"),
+      Tree("br6"),
+      Tree("br7"))))
 ]
