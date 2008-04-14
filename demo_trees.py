319a439c4c42ed3ba13aaf89fb870f5336c3c306
llimllib <llimllib@c4aad215-931b-0410-ba74-c4b90e0b6ad6>
2008-04-14 03:50:40 +0000

    adding another test tree... it demonstrates a bug
    git-svn-id: http://billmill.org/svn/personal_code/python/trees@173 c4aad215-931b-0410-ba74-c4b90e0b6ad6

--- demo_trees.py
+++ demo_trees.py
@@ -23,7 +23,7 @@ Tree("root",
         Tree("r4"))))
   ),
 
-#3 balanced
+#3 tight right
 Tree("root", 
   Tree("l1", 
     Tree("l2", 
@@ -75,7 +75,16 @@ Tree("root",
     Tree("rl"),
     Tree("rr"))),
 
-#6 buchheim n-ary tree
+#7 simple n-ary
+Tree("root",
+  Tree("l"),
+  Tree("m"),
+  Tree("r")),
+
+#8 buchheim n-ary tree
+#we see with this tree that spacing is not perfect; the middle subtrees tend
+#slightly towards the right. However, it fulfuills all the criteria set out
+#in the paper.
 Tree("root",
   Tree("bigleft",
     Tree("l1"),
@@ -85,9 +94,9 @@ Tree("root",
     Tree("l5"),
     Tree("l6"),
     Tree("l7", Tree("ll1"))),
-  Tree("r1"),
-  Tree("r2"),
-  Tree("r3", Tree("r31")),
+  Tree("m1"),
+  Tree("m2"),
+  Tree("m3", Tree("m31")),
   Tree("bigright",
     Tree("brr",
       Tree("br1"),
@@ -96,5 +105,39 @@ Tree("root",
       Tree("br4"),
       Tree("br5"),
       Tree("br6"),
-      Tree("br7"))))
+      Tree("br7")))),
+
+#9 random tree from http://www.brpreiss.com/books/opus5/html/page257.html
+# so... the "tree to the right thing" is actually a bug
+Tree("root",
+  Tree("E",
+    Tree("F",
+      Tree("F1"),
+      Tree("F2"),
+      Tree("F3")),
+    Tree("E2"),
+    Tree("E3")),
+  Tree("G",
+    Tree("H",
+      Tree("I",
+        Tree("I1"),
+        Tree("I2"),
+        Tree("I3")),
+      Tree("H2"),
+      Tree("H3")),
+    Tree("J",
+      Tree("K",
+        Tree("K1"),
+        Tree("K2"),
+        Tree("K3")),
+      Tree("L",
+        Tree("L1"),
+        Tree("L2"),
+        Tree("L3")),
+      Tree("J3")),
+    Tree("M",
+      Tree("M1"),
+      Tree("M2"),
+      Tree("M3"))),
+  Tree("D3")),
 ]
